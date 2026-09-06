b29d7049ecdc984a3c814d116bf583d1306e8758
Sujan S <168329664+sujan5s@users.noreply.github.com>
2026-09-06 15:35:16 +0530

    [678] [Time Beats: 100.00%] [Memory Beats: 92.94%] - LeetPush

---
+++ 678-valid-parenthesis-string/678-Valid-Parenthesis-String.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    bool checkValidString(string s) {
+        int maxi=0,mini=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                mini=mini+1;
+                maxi=maxi+1;
+            }else if(s[i]==')'){
+                mini=mini-1;
+                maxi=maxi-1;
+            }else{
+                mini=mini-1;
+                maxi=maxi+1;
+            }
+            if(mini<0){
+                mini=0;
+            }
+            if(maxi<0){
+                return false;
+            }
+        }
+        return (mini==0);
+    }
+};
