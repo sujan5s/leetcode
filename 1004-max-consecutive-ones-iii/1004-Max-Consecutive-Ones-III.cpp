9221409edab85abfc37c48a07279ca7252851f37
Sujan S <168329664+sujan5s@users.noreply.github.com>
2026-09-13 10:45:51 +0530

    [1004] [Time Beats: 32.94%] [Memory Beats: 5.13%] - LeetPush

--- 1004-max-consecutive-ones-iii/1004-Max-Consecutive-Ones-III.cpp
+++ 1004-max-consecutive-ones-iii/1004-Max-Consecutive-Ones-III.cpp
@@ -3,24 +3,24 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int len=0;
         int sum=0;
-        for(int i=0;i<nums.size();i++){
-            int zero=0;
-            int j=i;
-            while(j!=nums.size()){
-                if(nums[j]==0){
-                    zero++;
-                }
-                if(zero<=k){
-                    len=(j-i+1);
-                    sum=max(sum,len);
+        int zero=0;
+        int l=0,r=0;
+        while(r<nums.size()){
+            if(nums[r]==0){
+                zero++;
+            }
+            if(zero<=k){
+                len=(r-l+1);
+                sum=max(sum,len);
+            }else{
+                if(nums[l]==0){
+                    zero--;
+                    l++;
                 }else{
-                    break;
+                    l++;
                 }
-                j++;
             }
-            if(j==nums.size()){
-                break;
-            }
+            r++;
         }
         return sum;
     }
